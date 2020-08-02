91bc952d86f112ac3b7b92b1635b50cbeb285318
曾季暘(ShawnTseng) <shawntseng40@gmail.com>
2020-08-02 16:33:34 +0800

    add sheet

--- src/App.js
+++ src/App.js
@@ -3,13 +3,34 @@ import './App.css'
 
 function App() {
   const tablature = createRef();
-  const windowWidth = window.innerWidth * 0.9;
-  const windowHeight = window.innerHeight * 0.9;
+  const windowWidth = window.innerWidth * 0.95;
+  const windowHeight = window.innerHeight * 0.95;
+  const rowSpan = 150;
 
   useEffect(() => {
-    console.log('render');
-    const ctx = tablature.current.getContext('2d');
-    ctx.fillRect(0, 0, 100, 100);
+    const canvas = tablature.current.getContext('2d');
+    if (canvas) {
+      const drawLine = ({ x, y }, length) => {
+        canvas.beginPath();
+        canvas.moveTo(x, y);
+        canvas.lineTo(length, y);
+        canvas.stroke();
+      }
+      const addRow = startY => {
+        [...Array(6).keys()].forEach(count => {
+          drawLine({ x: 50, y: startY + (count * 10) }, 800);
+        })
+      }
+
+      const drawSheet = rowCount => {
+        [...Array(rowCount).keys()].forEach(count => {
+          addRow(50 + rowSpan * count);
+        })
+
+      }
+
+      drawSheet(4);
+    }
   }, [tablature])
 
   return (
